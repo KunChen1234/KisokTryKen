e9b7ead0c05c772cb3c1b197ef9689ca5cd5f041
ck <ckjohn0107@gmail.com>
2022-09-23 16:35:37 +1000

    Update databaseTest.ts

--- ceshi/databaseTest.ts
+++ ceshi/databaseTest.ts
@@ -3,14 +3,14 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  const users = await prisma.user.findMany(
-    {
-      where:{
-        id=1
-      }
-    }
-  )
-  console.log(users)
+  // const users = await prisma.user.findMany(
+  //   {
+  //     where:{
+  //       id=1
+  //     }
+  //   }
+  // // )
+  // console.log(users)
 }
 
 main()
